885078613dc6d64e9749ed8758e85d7df656bc46
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2021-11-09 19:22:58 +0700

    create model

---
+++ app/src/main/java/Model/NOTIFICATION.java
@@ -0,0 +1,39 @@
+package Model;
+
+import java.io.Serializable;
+
+public class NOTIFICATION implements Serializable {
+    int imgLogo;
+    String title;
+    String content;
+
+    public int getImgLogo() {
+        return imgLogo;
+    }
+
+    public void setImgLogo(int imgLogo) {
+        this.imgLogo = imgLogo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+
+
+
+}
