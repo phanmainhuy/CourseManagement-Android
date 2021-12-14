153c61e2c3d02dc51a6979df15a4995a4c74b097
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2021-12-15 01:36:50 +0700

    Thiết kế xong layout 1 dòng cho khuyến mãi khách hàng, chuẩn bị viết adapter

---
+++ app/src/main/java/com/example/onlearn/models/KM_KH.java
@@ -0,0 +1,82 @@
+package com.example.onlearn.models;
+
+import java.io.Serializable;
+
+public class KM_KH implements Serializable {
+    public int MaKM;
+    public int MaHV;
+    public String TenKM;
+    public String imgKM;
+    public String GiaTri;
+    public String MaApDung;
+    public String HSD;
+
+    public KM_KH(int maKM, int maHV, String tenKM, String imgKM, String giaTri, String maApDung, String HSD) {
+        MaKM = maKM;
+        MaHV = maHV;
+        TenKM = tenKM;
+        this.imgKM = imgKM;
+        GiaTri = giaTri;
+        MaApDung = maApDung;
+        this.HSD = HSD;
+    }
+
+    public int getMaKM() {
+        return MaKM;
+    }
+
+    public void setMaKM(int maKM) {
+        MaKM = maKM;
+    }
+
+    public int getMaHV() {
+        return MaHV;
+    }
+
+    public void setMaHV(int maHV) {
+        MaHV = maHV;
+    }
+
+    public String getTenKM() {
+        return TenKM;
+    }
+
+    public void setTenKM(String tenKM) {
+        TenKM = tenKM;
+    }
+
+    public String getImgKM() {
+        return imgKM;
+    }
+
+    public void setImgKM(String imgKM) {
+        this.imgKM = imgKM;
+    }
+
+    public String getGiaTri() {
+        return GiaTri;
+    }
+
+    public void setGiaTri(String giaTri) {
+        GiaTri = giaTri;
+    }
+
+    public String getMaApDung() {
+        return MaApDung;
+    }
+
+    public void setMaApDung(String maApDung) {
+        MaApDung = maApDung;
+    }
+
+    public String getHSD() {
+        return HSD;
+    }
+
+    public void setHSD(String HSD) {
+        this.HSD = HSD;
+    }
+
+
+
+}
