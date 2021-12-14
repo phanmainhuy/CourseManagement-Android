255f06f034b4d2eb5dc4fd9a89ed3a1b2bedb8ae
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2021-12-15 00:14:12 +0700

    add format date

--- app/src/main/java/com/example/onlearn/utils/utils.java
+++ app/src/main/java/com/example/onlearn/utils/utils.java
@@ -1,6 +1,9 @@
 package com.example.onlearn.utils;
 
 import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class utils {
 
@@ -10,5 +13,18 @@ public class utils {
         DecimalFormat format = new DecimalFormat("#,###");
         return format.format(Double.parseDouble(gia));
     }
+
+    public static Date formatStringtoDate(String datestring) {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date = null;
+        try {
+            date = df.parse(datestring);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+
 
 }
