0918c9a0f0fb6832256f9842b3ccb71317dea9f0
nhuy0511 <nhuy@vn-cubesystem.com>
2022-01-09 16:27:28 +0700

    update status bar

--- app/src/main/java/com/example/onlearn/activity/register/RegisterActivity.java
+++ app/src/main/java/com/example/onlearn/activity/register/RegisterActivity.java
@@ -2,14 +2,19 @@ package com.example.onlearn.activity.register;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -31,6 +36,7 @@ import java.util.Map;
 public class RegisterActivity extends AppCompatActivity {
 
     String urlPostRegist = GLOBAL.ip + "api/Identity/student";
+    TextView tvValidation;
 
     Button btnCancel, btnRegist;
     EditText txtUserName, txtPassword, txtRePass;
@@ -49,6 +55,19 @@ public class RegisterActivity extends AppCompatActivity {
         context = getApplicationContext();
         api = new API(RegisterActivity.this);
 
+        //Change status bar
+    Activity activity = RegisterActivity.this;
+    Window window = activity.getWindow();
+
+    // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+    // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+    // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(activity, R.color.statusBar_register));
+
 
         //anh xa
         btnCancel = findViewById(R.id.btnCancel_Register);
@@ -56,6 +75,8 @@ public class RegisterActivity extends AppCompatActivity {
         txtUserName = findViewById(R.id.txt_username_Register);
         txtPassword = findViewById(R.id.txt_password_Register);
         txtRePass = findViewById(R.id.txt_repass_Register);
+        tvValidation = findViewById(R.id.tvValidate_Register);
+
 
 
         //Onclick
@@ -66,24 +87,29 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         btnRegist.setOnClickListener(v -> {
-            if (txtUserName.getText().toString().equals("")) {
-                Toast.makeText(context, "Vui lòng không bỏ trống tên đăng nhập", Toast.LENGTH_SHORT).show();
+            if (txtUserName.getText().toString().trim().equals("")) {
+//                Toast.makeText(context, "Vui lòng không bỏ trống tên đăng nhập", Toast.LENGTH_SHORT).show();
+                tvValidation.setText("Vui lòng không bỏ trống tên đăng nhập");
                 return;
             }
-            if (txtRePass.getText().toString().equals("") || txtPassword.getText().toString().equals("")) {
-                Toast.makeText(context, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
+            if (txtRePass.getText().toString().trim().equals("") || txtPassword.getText().toString().trim().equals("")) {
+//                Toast.makeText(context, "Vui lòng nhập mật khẩu", Toast.LENGTH_SHORT).show();
+                tvValidation.setText("Vui lòng nhập mật khẩu");
                 return;
             }
             if (!txtPassword.getText().toString().equals(txtRePass.getText().toString())) {
-                Toast.makeText(context, "Mật khẩu nhập lại không khớp", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, "Mật khẩu nhập lại không khớp", Toast.LENGTH_SHORT).show();
+                tvValidation.setText("Mật khẩu nhập lại không khớp");
                 return;
             }
             if (txtUserName.getText().toString().length() < 6) {
-                Toast.makeText(context, "Tên đăng nhập phải trên 6 ký tự\nBạn đang nhập " + txtUserName.getText().toString().length() + " kí tự", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, "Tên đăng nhập phải trên 6 ký tự\nBạn đang nhập " + txtUserName.getText().toString().length() + " kí tự", Toast.LENGTH_SHORT).show();
+                tvValidation.setText("Tên đăng nhập phải trên 6 ký tự\nBạn đang nhập " + txtUserName.getText().toString().length() + " kí tự");
                 return;
             }
             if (txtPassword.getText().toString().length() < 6) {
-                Toast.makeText(context, "Mật khẩu phải trên 6 ký tự\nBạn đang nhập " + txtPassword.getText().toString().length() + " kí tự", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, "Mật khẩu phải trên 6 ký tự\nBạn đang nhập " + txtPassword.getText().toString().length() + " kí tự", Toast.LENGTH_SHORT).show();
+                tvValidation.setText("Mật khẩu phải trên 6 ký tự\nBạn đang nhập " + txtPassword.getText().toString().length() + " kí tự");
                 return;
             } else {
                 try {
